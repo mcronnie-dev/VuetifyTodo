6cc64fcab67a3077bbc3f2ee2366bdcfabb9bd83
mcronnie-dev <mcronnie.dev@gmail.com>
2020-12-11 19:55:41 +0800

    Add About content, and padding

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat neque, blandit varius justo at, vestibulum commodo est. Phasellus nulla sem, maximus vel accumsan eget, aliquet non arcu. Mauris ultrices odio sit amet nibh posuere tristique. Nullam in viverra felis.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat neque, blandit varius justo at, vestibulum commodo est. Phasellus nulla sem, maximus vel accumsan eget, aliquet non arcu. Mauris ultrices odio sit amet nibh posuere tristique. Nullam in viverra felis.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat neque, blandit varius justo at, vestibulum commodo est. Phasellus nulla sem, maximus vel accumsan eget, aliquet non arcu. Mauris ultrices odio sit amet nibh posuere tristique. Nullam in viverra felis.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat neque, blandit varius justo at, vestibulum commodo est. Phasellus nulla sem, maximus vel accumsan eget, aliquet non arcu. Mauris ultrices odio sit amet nibh posuere tristique. Nullam in viverra felis.</p>
   </div>
 </template>
