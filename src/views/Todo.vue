1b5a6b5460a80e5b0a640069e0ed9692827ed3f6
mcronnie-dev <mcronnie.dev@gmail.com>
2020-12-12 02:17:46 +0800

    Add Textfield to add new task

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,44 +1,55 @@
 <template>
   <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+    ></v-text-field>
     <v-list
       class="pt-0"
       flat
     >
-    <div
-      v-for="task in tasks"
-      :key="task.id"
-    >
-      <v-list-item
-        @click="doneTask(task.id)"
-        :class="{ 'blue lighten-5' : task.done}"
+      <div
+        v-for="task in tasks"
+        :key="task.id"
       >
-        <template v-slot:default>
-          <v-list-item-action>
-            <v-checkbox
-              :input-value="task.done"
-              color="primary"
-            ></v-checkbox>
-          </v-list-item-action>
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5' : task.done}"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="task.done"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title
-              :class="{'text-decoration-line-through': task.done}"
-            >
-              {{task.title}}
-            </v-list-item-title>
-          </v-list-item-content>
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{'text-decoration-line-through': task.done}"
+              >
+                {{task.title}}
+              </v-list-item-title>
+            </v-list-item-content>
 
-          <v-list-item-action>
-            <v-btn icon
-              @click.stop="deleteTask(task.id)"
-            >
-              <v-icon color="primary">mdi-delete</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </template>
-      </v-list-item>
-      <v-divider></v-divider>
-    </div>
+            <v-list-item-action>
+              <v-btn icon
+                @click.stop="deleteTask(task.id)"
+              >
+                <v-icon color="primary">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
     </v-list>
   </div>
 </template>
@@ -48,26 +59,36 @@ export default {
   name: 'Home',
   data() {
     return {
+      newTaskTitle: '',
       tasks: [
-        {
-          id: 1,
-          title: 'Wake up',
-          done: false
-        },
-        {
-          id: 2,
-          title: 'Get bananas',
-          done: true
-        },
-        {
-          id: 3,
-          title: 'Eat bananas',
-          done: false
-        }
+        // {
+        //   id: 1,
+        //   title: 'Wake up',
+        //   done: false
+        // },
+        // {
+        //   id: 2,
+        //   title: 'Get bananas',
+        //   done: true
+        // },
+        // {
+        //   id: 3,
+        //   title: 'Eat bananas',
+        //   done: false
+        // }
       ]
     }
   },
   methods:{
+    addTask(){
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    },
     doneTask(id) {
       let task = this.tasks.filter(task => task.id === id)[0]
       task.done = !task.done
