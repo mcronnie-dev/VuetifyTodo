fd1353f4a902fa28d236c354e4bb90d1a5dbd9b6
mcronnie-dev <mcronnie.dev@gmail.com>
2020-12-13 21:23:33 +0800

    Update About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,6 +4,7 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat neque, blandit varius justo at, vestibulum commodo est. Phasellus nulla sem, maximus vel accumsan eget, aliquet non arcu. Mauris ultrices odio sit amet nibh posuere tristique. Nullam in viverra felis.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat neque, blandit varius justo at, vestibulum commodo est. Phasellus nulla sem, maximus vel accumsan eget, aliquet non arcu. Mauris ultrices odio sit amet nibh posuere tristique. Nullam in viverra felis.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat neque, blandit varius justo at, vestibulum commodo est. Phasellus nulla sem, maximus vel accumsan eget, aliquet non arcu. Mauris ultrices odio sit amet nibh posuere tristique. Nullam in viverra felis.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat neque, blandit varius justo at, vestibulum commodo est. Phasellus nulla sem, maximus vel accumsan eget, aliquet non arcu. Mauris ultrices odio sit amet nibh posuere tristique. Nullam in viverra felis.</p>
+    <p>Project based on <a href="https://youtu.be/CjXgoYo86yY" target="_blank">Vuetify: Create an App with Vue JS - in 30 MINUTES!</a></p>
+    <p>Check me out <a href="https://github.com/mcronnie-dev" target="_blank">@mcronnie-dev</a></p>
   </div>
 </template>
